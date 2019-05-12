d50790abbd89712d13399e37623e435cbcb0022f
Scott Richardson <srich@Scotts-MacBook-Pro.local>
2019-05-12 00:36:19 -0700

    update .gitignore

--- app/dist/sw.js
+++ app/dist/sw.js
@@ -2,8 +2,8 @@ var serviceWorkerOption = {
   "assets": [
     "/0.css",
     "/0.bundle.js",
-    "/1.bundle.js",
     "/2.bundle.js",
+    "/3.bundle.js",
     "/4.bundle.js",
     "/5.bundle.js",
     "/6.bundle.js",
